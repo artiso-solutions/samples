c6d16064234df4fc469ee180f9a84a85de099ec0
Thomas Schissler <tschissler@artiso.com>
2015-03-31 21:59:49 +0200

    Added missing files

---
+++ LiveUpdate/DashboardService.Contracts/IDashboardContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace DashboardContracts
+{
+    [ServiceContract]
+    public interface IDashboardContract
+    {
+        [OperationContract]
+        void NotifyUpdatedVersion(string component, string currentVersion);
+
+        [OperationContract]
+        void NotifyFallback(string component, string currentVersion, Exception exception);
+    }
+}
