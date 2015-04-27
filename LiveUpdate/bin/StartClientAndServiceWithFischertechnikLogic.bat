5f4b48282f47e6e22c988d10996a2dad9c7e6463
christine schneidenwind <cschneidenwind@artiso.com>
2015-04-27 10:59:25 +0200

    $add batch files to make it easier to start applications

---
+++ LiveUpdate/bin/StartClientAndServiceWithFischertechnikLogic.bat
@@ -0,0 +1,6 @@
+start "" ".\ServiceHost\ConsoleHost.exe" "UseFischertechnikController"
+#sleep for 4 seconds; this works for all windows versions.
+ping -n <4> 127.0.0.1 > NUL
+start "" ".\Client\Shell.exe"
+#sleep for 2 seconds; this works for all windows versions.
+ping -n <2> 127.0.0.1 > NUL
